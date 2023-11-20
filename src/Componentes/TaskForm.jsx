108fd6c72b83d2020ef5a88d24047c21be30271e
ezedesa <ezequiel.194300@gmail.com>
2023-11-20 15:28:14 -0300

    css test background with gradient and keyframes

--- src/Componentes/TaskForm.jsx
+++ src/Componentes/TaskForm.jsx
@@ -1,15 +1,14 @@
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import '../../src/App.css';
 
 function TaskForm() {
   return (
     <>
       <InputGroup className="mb-3">
-        <InputGroup.Text id="inputGroup-sizing-default">
-          Task
-        </InputGroup.Text>
-        <Form.Control
-          aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+        <InputGroup.Text id="inputGroup-sizing-default" placeholder='Introduce una tarea'>Task</InputGroup.Text>
+        <Form.Control aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+        <input type="submit" value="Add task" className='btn'/>
       </InputGroup>
     </>
   );
