b5931510fd631af83be9ce2cdc70eb682335822c
ezedesa <ezequiel.194300@gmail.com>
2023-11-22 20:45:15 -0300

    add event onClick, onChange and function handleDelete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-/*import reactLogo from './assets/react.svg'*/
 import './App.css'
 import TaskForm from './Componentes/TaskForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <TaskForm />
     </>
-  )
+  );
 }
-export default App
+export default App;
--- src/Componentes/TaskForm.jsx
+++ src/Componentes/TaskForm.jsx
@@ -1,16 +1,27 @@
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import '../../src/App.css';
+import {useState} from 'react'
 
-function TaskForm() {
-  return (
-    <>
-      <InputGroup className="mb-3">
-        <InputGroup.Text id="inputGroup-sizing-default" placeholder='Introduce una tarea'>Task</InputGroup.Text>
-        <Form.Control aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
-        <input type="submit" value="Add task" className='btn'/>
-      </InputGroup>
-    </>
-  );
+function TaskForm (){
+    const [tarea,setTarea]=useState('');
+
+    function handleClick() {
+        console.log('Se capturó el evento onClick y se guardó el valor ingresado en el input, la tarea es:', tarea);
+    }
+    function handleDelete() {
+        setTarea('');
+        console.log('Se setearon todos los campos mediante el evento onClick y la función handleDelete')
+    }
+    function handleEdit(){
+        pass
+    }
+    return(
+        <>
+            <div>
+                Tarea<input type="text" value={tarea} onChange={ (e) => setTarea(e.target.value) } placeholder='Escriba una tarea'/>
+                <button onClick={handleClick} className='btn'>Guardar</button>
+                <button onClick={handleEdit} className='btn'>Editar</button>
+                <button type='reset' onClick={handleDelete} className='btn'>Eliminar</button>
+            </div>
+        </>
+    );
 }
 export default TaskForm;
