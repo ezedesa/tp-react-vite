6fc4ff0aad7d7f5266ab09405dbe961e47912fce
sergiocardozo <scardozo.sc@gmail.com>
2023-11-23 11:18:06 -0300

    TaskList component update front

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 /*import reactLogo from './assets/react.svg'*/
 import './App.css'
 import TaskForm from './Componentes/TaskForm'
+import TaskList from './Componentes/TaskList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <TaskForm />
+      <TaskList />
     </>
   )
 }
