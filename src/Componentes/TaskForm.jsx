77a1df09a23a12395c86180733373e83bfc672fd
ezedesa <ezequiel.194300@gmail.com>
2023-11-24 20:28:08 -0300

    takform add functions

--- src/Componentes/TaskForm.jsx
+++ src/Componentes/TaskForm.jsx
@@ -2,8 +2,10 @@ import {useState} from 'react'
 
 function TaskForm (){
     const [tarea,setTarea]=useState('');
+    const [tareag,setTareag]=useState('');
 
     function handleClick() {
+        {tarea !="" ? <p>{tarea}</p>:alert("Debe ingresar una tarea para poder guardar")}
         console.log('Se capturó el evento onClick y se guardó el valor ingresado en el input, la tarea es:', tarea);
     }
     function handleDelete() {
@@ -16,10 +18,11 @@ function TaskForm (){
     return(
         <>
             <div>
-                Tarea<input type="text" value={tarea} onChange={ (e) => setTarea(e.target.value) } placeholder='Escriba una tarea'/>
+                Tarea<input type="text" onChange={ (e) => setTarea(e.target.value) } value={tarea} placeholder='Escriba una tarea' required/>
                 <button onClick={handleClick} className='btn'>Guardar</button>
                 <button onClick={handleEdit} className='btn'>Editar</button>
                 <button type='reset' onClick={handleDelete} className='btn'>Eliminar</button>
+                <p>{tarea}</p>
             </div>
         </>
     );
